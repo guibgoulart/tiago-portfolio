e45add058153b54d5f7c902af8039698a269c8f0
Guilherme Goulart <ggoulart@truedialog.com>
2024-07-06 23:11:31 -0300

    feat: create introcomponent

---
+++ src/components/Intro/IntroComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container mx-auto p-4 font-playfair">
+        <div class="bg-black text-white p-6 rounded-lg shadow-lg">
+            <div class="flex flex-col md:flex-row justify-between items-center">
+                <div class="text-left md:ml-4">
+                    <p class="text-lg md:text-3xl mb-2">
+                        Hello, are you okay? 🛸
+                    </p>
+                    <p class="text-sm md:text-xl mb-4">
+                        This is my thirteenth attempt to craft an engaging and unique portfolio with the aim of making a
+                        strong impression and securing a position with your esteemed organization.
+                        <br />
+                        I trust you will appreciate the effort and creativity I have put into it as much as I have
+                        enjoyed the process.
+                    </p>
+                    <p class="text-lg md:text-3xl">Peace! ✊🏽</p>
+                    <div class="flex justify-center my-4 md:my-0">
+                        <img src="@/assets/what-huh.gif" alt="Confused Travolta" class="rounded-lg shadow-lg" />
+                    </div>
+                </div>
+            </div>
+            <div class="text-center mt-4">
+                <p class="text-lg md:text-3xl text-white md:text-red">ceci n'est pas un portefeuille.</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'IntroComponent'
+}
+</script>
+
+<style scoped></style>
