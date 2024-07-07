f751edd19e81e83307ce64408d38f5ce7cab8a03
Guilherme Goulart <ggoulart@truedialog.com>
2024-07-06 23:11:19 -0300

    feat: create quotecomponent

---
+++ src/components/Quote/QuoteComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex items-center justify-center container mx-auto p-4 font-playfair">
+        <div class="bg-white text-black p-6 rounded-lg shadow-lg">
+            <div class="flex flex-col md:flex-row justify-between items-center">
+                <div class="text-center md:ml-4">
+                    <p class="text-lg md:text-3xl mb-2">
+                        "La vida no es sino una continua sucesión de oportunidades para sobrevivir."
+                    </p>
+                    <p class="text-sm md:text-xl mb-4">
+                        Gabriel García Márquez
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'QuoteComponent'
+}
+</script>
+
+<style scoped></style>
