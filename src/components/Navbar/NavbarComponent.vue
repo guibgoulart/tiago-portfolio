17277bdb8be7d936f0d587c4cb3dbb244b4fd286
Guilherme Goulart <ggoulart@truedialog.com>
2024-07-06 23:11:48 -0300

    feat: create navbar component

---
+++ src/components/Navbar/NavbarComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="container mx-auto flex flex-row justify-between items-center font-helvetica text-black text-[1.5em] md:text-[3.75em]">
+    <div class="ml-4 text-left">
+      <p class="leading-tight">tiago</p>
+      <p class="leading-tight">goulart</p>
+    </div>
+    <div class="text-center">
+      <p class="leading-tight">ux·ui</p>
+      <p class="leading-tight">design</p>
+    </div>
+    <div class="mr-4 text-right">
+      <p class="leading-tight">graphic</p>
+      <p class="leading-tight">design</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent'
+}
+</script>
+
+<style scoped></style>
