2538b5b06f487405ce90b78b5fa2b127b02c4857
Leonardo Santos da Costa <lsdcostaaa@gmail.com>
2024-08-15 15:57:23 -0300

    deixei responsivo

---
+++ src/components/Work/WorkComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container mt-8 font-playfair">
+      <!-- UX/UI Design Section -->
+      <div class="bg-yellow-200 p-6 rounded-lg">
+        <h2 class="text-2xl md:text-4xl font-bold mb-4">ux-ui design</h2>
+        <div class="flex flex-col md:flex-row mb-6">
+          <div class="flex-1 mb-4 md:mb-0">
+            <img src="@/assets/Mockup.png" alt="Petpals Case Study" class="rounded-lg" />
+          </div>
+          <div class="flex-1 md:pl-4">
+            <p class="text-lg md:text-xl">Case Study: 2024 Master in UX/UI Design Program at Nuclio Digital School, Barcelona.</p>
+          </div>
+        </div>
+        <div class="flex flex-col md:flex-row">
+          <div class="flex-1 mb-4 md:mb-0">
+            <img src="@/assets/Card Vas.png" alt="Vas Case Study" class="rounded-lg" />
+          </div>
+          <div class="flex-1 md:pl-4">
+            <p class="text-lg md:text-xl">Case Study: 2024.</p>
+          </div>
+        </div>
+      </div>
+  
+      <!-- Graphic Design Section -->
+      <div class="bg-yellow-300 p-6 rounded-lg mt-6">
+        <h2 class="text-2xl md:text-4xl font-bold mb-4">graphic design</h2>
+        <div class="flex flex-col md:flex-row items-center">
+          <div class="flex-1 mb-4 md:mb-0">
+            <img src="@/assets/Card aue.png" alt="Associação Brasil Aué" class="rounded-lg" />
+          </div>
+          <div class="flex-1 md:pl-4">
+            <p class="text-lg md:text-xl">Branding, Logo and Visual Identity Design for Associação Brasil Aué, 2023, Barcelona.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DesignShowcaseComponent'
+  }
+  </script>
+  
+  <style scoped></style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import NavbarComponent from '@/components/Navbar/NavbarComponent.vue';
 import IntroComponent from '@/components/Intro/IntroComponent.vue';
 import QuoteComponent from '@/components/Quote/QuoteComponent.vue';
 import BioComponent from '@/components/Bio/BioComponent.vue';
+import WorkComponent from '@/components/Work/WorkComponent.vue';
 </script>
 
 <template>
@@ -11,5 +12,7 @@ import BioComponent from '@/components/Bio/BioComponent.vue';
         <IntroComponent />
         <BioComponent />
         <QuoteComponent />
+        <WorkComponent />
+        
     </div>
 </template>
