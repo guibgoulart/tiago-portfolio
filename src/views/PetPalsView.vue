aec7083866cb3703656489a575b002c0fec19539
Leonardo Santos da Costa <lsdcostaaa@gmail.com>
2024-09-04 18:10:55 -0300

    seguimento no bg red do petpals, sem sucesso nos textos, necessita consertar bugs

--- src/views/PetPalsView.vue
+++ src/views/PetPalsView.vue
@@ -34,6 +34,22 @@
       />
     </div>
   </div>
+  
+  <!-- Red Section with Image and Text -->
+  <div class="bg-red-500 p-8 flex flex-col items-center bg-red">
+    <img
+      src="@/assets/petpals/Group_15460.png"
+      alt="Image"
+      class="w-full max-w-xs sm:max-w-3xl"
+    />
+    <h1 class="mt-4 text-center text-white font-bold text-2xl sm:text-3xl">
+      In this initial phase of UX research, employing empathy through interviews with potential users, we observed that their primary pain points were finding a trustworthy caregiver, having more time to travel, and finding affordable care options.
+    </h1>
+  
+    <h2 class="mt-4 text-center text-white font-bold text-2xl sm:text-3xl">
+      Addressing these issues would increase travel opportunities, reduce the stress of searching, facilitate access to care, enhance socialization with neighbors, and provide users with greater financial savings.
+    </h2>
+  </div>
 </template>
 
 <script setup>
