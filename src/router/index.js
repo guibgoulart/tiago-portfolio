6c7d85d039ef22b4b192e8926caab69f13dcaea2
Leonardo Santos da Costa <lsdcostaaa@gmail.com>
2024-10-15 16:46:30 -0300

    atualização do codigo para os botoes da nav bar fazerem os redirecionamentos para as devidas paginas, falta a parte dos projects

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -21,6 +21,12 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
+
+    },
+    {
+      path: '/contactme',
+      name: 'contacme',
+      component: () => import('../views/ContactMeView.vue')
 
     }
   ]
--- src/views/ContactMeView.vue
+++ src/views/ContactMeView.vue
@@ -1,15 +1,12 @@
-
 <script setup>
 import NewNavbar from '@/components/Navbar/NewNavbar.vue';
 import FooterComponent2 from '@/components/Footer/FooterComponent2.vue';
-import AboutComponent from '@/components/About/AboutComponent.vue';
 import ContactMeComponent from '@/components/ContactMe/ContactMeComponent.vue';
 </script>
 
 <template>
     <div>
         <NewNavbar />
-        <AboutComponent />
         <ContactMeComponent />
         <FooterComponent2 />
     </div>
