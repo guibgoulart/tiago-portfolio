9723ebddaa6a1104cdf94793fdb92a762c48e26b
guibg <gui.bgoulart@gmail.com>
2024-12-10 17:55:04 -0300

    iphone compatibility

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -12,27 +12,26 @@ const router = createRouter({
     {
       path: '/petpals',
       name: 'petpals',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/PetPalsView.vue')
     },
     {
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
-
     }
   ],
   scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
     if (to.hash) {
       return {
         el: to.hash,
-        behavior: 'smooth',
-      };
+        behavior: 'instant'
+      }
     }
-    return { top: 0 };
-  },
+    return { top: 0, behavior: 'instant' }
+  }
 })
 
 export default router
