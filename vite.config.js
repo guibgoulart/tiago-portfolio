cd611769c7f8f9d247911fa8154d63b82385d85d
guibg <gui.bgoulart@gmail.com>
2024-12-18 15:23:28 -0300

    update vite version

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'last 2 versions', 'safari >= 12'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
